6255bdd25738f5b7807db5a9a68453533f2d951a
illumination-k <illumination.k.27@gmail.com>
2022-08-27 21:11:22 +0900

    update to webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,4 +2,7 @@ module.exports = {
   "stories": [
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+  core: {
+    builder: "webpack5",
+  },
 };
---
+++ components/atoms/CircleButton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { apply, tw } from "twind";
+
+export type CircleButtonProps = React.ComponentProps<"button">;
+
+const CircleButton = ({ className, children, color, ...props }: CircleButtonProps) => {
+  return (
+    <button
+      className={tw(
+        apply(
+          "font-bold font-sans rounded-full w-16 h-16 text-white bg-blue-600",
+          {
+            hover: "bg-blue-500",
+            focus: "outline-none focus:ring ring-blue-300",
+          },
+          className,
+        ),
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CircleButton;
